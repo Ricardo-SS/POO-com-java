83a7ed31a29418f2e5bf2fe9558985725fd25dab
Francisco Ricardo Dos Santos Silva <98191651+Ricardo-SS@users.noreply.github.com>
2024-05-02 15:30:33 -0300

    Atualizar o Calendario.java

--- QuestoesPOO-main/7. Exceções/Questao57/Calendario.java
+++ QuestoesPOO-main/7. Exceções/Questao57/Calendario.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+
 public class Calendario {
     public static void main(String[] args) {
-        indentificaMes(13);
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o número do mês (1 a 12): ");
+        int numMes = input.nextInt();
+
+        identificaMes(numMes);
+
+        input.close();
     }
-    
-    public static void indentificaMes(int numMes){
+
+    public static void identificaMes(int numMes) {
         String[] meses = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
 
         try {
-            System.out.println(meses[numMes-1]);
+            System.out.println("O mês correspondente é: " + meses[numMes - 1]);
         } catch (Exception e) {
-            System.out.println(e);
             System.out.println("Este mês não existe!!!");
         }
     }
